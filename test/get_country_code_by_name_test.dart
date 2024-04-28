8516eb39014a60a88b111e980ab13b087ef7f793
Mohamed Hekal <mohamedhekal201414@gmail.com>
2024-04-28 15:07:31 +0300

    update 3

--- test/get_country_code_by_name_test.dart
+++ test/get_country_code_by_name_test.dart
@@ -1,16 +1,15 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:get_country_code_by_name/get_country_code_by_name.dart';
 
 void main() {
-  group('CountryCodeResolver', () {
-    test('getCountryCode returns correct country code', () {
-      var resolver = CountryCodeResolver();
-      expect(resolver.getCountryCode('egypt'), equals('eg'));
-    });
+  var resolver = CountryCodeResolver();
 
-    test('getCountryCode returns Unknown for invalid country name', () {
-      var resolver = CountryCodeResolver();
-      expect(resolver.getCountryCode('invalid'), equals('Unknown'));
-    });
-  });
+  // Retrieve country code for a given country name
+  String countryName = 'egypt';
+  String countryCode = resolver.getCountryCode(countryName);
+  print('Country code for $countryName: $countryCode');
+
+  // Retrieve country name for a given country code
+  String countryCode2 = 'eg';
+  String countryName2 = resolver.getCountryName(countryCode2);
+  print('Country name for $countryCode2: $countryName2');
 }
